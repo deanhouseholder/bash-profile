c3d54a9e54819f30f301e91edb1aedd1a6bee91b
Dean Householder <dhouseholder@infoarmor.com>
2021-02-25 21:05:32 -0700

    Made search much faster and now supports all characters

--- include/search_shortcuts.sh
+++ include/search_shortcuts.sh
@@ -2,6 +2,7 @@
 # $1 = Search string
 # $2 = (optional) File pattern (ex: *.js) (default is: *)
 # $3 = (optional) Set to 1 for case-insensitive search (default is: case-sensitive)
+# Bug: Backslashes are not displaying in results. (ex: "\n" shows up as "n")
 search(){
   # Define Vars
   local sep=$'\x01' # Obscure ascii character not likely to appear in files
@@ -10,11 +11,12 @@ search(){
   local end='\e[0m'
   local green='\e[32m'
   local purple='\e[35m'
+  local start_red_hex='1b5b313b33316d' # printf '\e[1;31m' | xxd -p
+  local stop_red_hex='1b5b306d'        # printf '\e[0m' | xxd -p
   local filter_swap_separators="s/^([^:]*):([^:]+):\s*(.*)$/\2$sep\1$sep\3/g"
-  local filter_out_colors="\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]"
   local col_line_w=0 # Column containing the line number's max width
   local col_path_w=0 # Column containing the file path's max width
-  local search name case_sensitive find col_line col_path col_data error message usage
+  local search name case_sensitive find_array col_line col_path col_data error message usage
 
   # Check for missing input
   if [[ -z "$1" ]]; then
@@ -37,22 +39,27 @@ search(){
   test -z "$2" && name='*' || name="$2"
   test "$3" == '1' && case_sensitive='i'
 
-  # Run find command can capture the results into an array
-  mapfile find < <( \
-    find . -type f -name "$name" -exec grep -${case_sensitive}nH --color=always "$search" {} \; \
+  # Escape any semicolons in search input for safety with grep
+  local escaped_search="$(printf "%s" "$search" | sed -e 's/;/\\;/g')"
+
+  # To avoid difficulty allowing all search characters without sed confusing them for regex characters,
+  # convert search input to hex characters where replace is simple.
+  local search_hex="$(printf "%s" "$search" | xxd -p)" # Convert search into hex
+  local replace_hex="$start_red_hex$search_hex$stop_red_hex"  # Build replacement string in hex
+
+  # Run find command and capture the results into an array
+  mapfile find_array < <( \
+    find . -type f -name "$name" -exec grep -${case_sensitive}nH --fixed-strings "$escaped_search" {} \; \
     | grep -v '^Binary' | uniq | sed -r -e "$filter_swap_separators" \
   )
 
   # loop through the first time to determine max column widths
   while read line; do
     while IFS="$sep" read -r col_line col_path col_data; do
-      # Strip out color characters everywhere
-      col_line_plain="$(echo "$col_line" | sed -r "s/$filter_out_colors//g")"
-      col_path_plain="$(echo "$col_path" | sed -r "s/$filter_out_colors//g")"
-      if [[ $col_line_w -lt ${#col_line_plain} ]]; then col_line_w=${#col_line_plain}; fi
-      if [[ $col_path_w -lt ${#col_path_plain} ]]; then col_path_w=${#col_path_plain}; fi
+      if [[ $col_line_w -lt ${#col_line} ]]; then col_line_w=${#col_line}; fi
+      if [[ $col_path_w -lt ${#col_path} ]]; then col_path_w=${#col_path}; fi
     done < <(echo "${line[@]}")
-  done < <(echo "${find[@]}")
+  done < <(echo "${find_array[@]}")
 
   # Add some padding
   let col_line_w+=$col_spacing
@@ -62,28 +69,37 @@ search(){
   let col_path_w+=$col_spacing
 
   # Print heading
-  printf "${bold}%-${col_line_w}s%-${col_path_w}s%s${end}\n" "Line" "File Path" "Search Results"
-  printf "${bold}%-${col_line_w}s%-${col_path_w}s%s${end}\n" "----" "---------" "---------------"
+  printf "\n${bold}%-${col_line_w}s%-${col_path_w}s%s${end}\n" "Line" "File Path" "Search Results"
+  printf   "${bold}%-${col_line_w}s%-${col_path_w}s%s${end}\n" "----" "---------" "---------------"
 
   # Loop through again to display output in columns
   while read line; do
     while IFS="$sep" read -r col_line col_path col_data; do
-      col_line=$(echo "$col_line" | sed -r "s/$filter_out_colors//g")         # Strip out color codes everywhere
-      col_path=$(echo "$col_path" | sed -r "s/$filter_out_colors//g")         # Strip out color codes everywhere
-      col_data=$(echo "$col_data" | sed -r "s/^($filter_out_colors)*//g")     # Trim leading color codes only
-      col_data=$(echo "$col_data" | sed -r -e 's/^[ \t]*//' -e 's/[ \t]*$//') # Trim leading/trailing spaces
+      # Add color to search string in results (Do search/replace in hex mode and then swap back)
+      col_data="$(printf "%s" "$col_data" | xxd -p | sed "s/$search_hex/$replace_hex/g" | xxd -p -r)"
       printf "${green}%-${col_line_w}s$end${purple}%-${col_path_w}s$end%s\n" "$col_line" "$col_path" "${col_data//^\w/}"
     done < <(echo "${line[@]}")
-  done < <(echo "${find[@]}")
+  done < <(echo "${find_array[@]}")
 }
 
-sphp(){ search "$1" '*.php'; }
-sphpi(){ search "$1" '*.php' 1; }
-scss(){ search "$1" '*.css'; }
-scssi(){ search "$1" '*.css' 1; }
-sjs(){ search "$1" '*.js'; }
-sjsi(){ search "$1" '*.js' 1; }
-searchall(){ search "$1" '*'; }
-searchalli(){ search "$1" '*' 1; }
-searchcount(){ echo; printf "\nMatches\tFilename\n-----\t--------------------------------\n$(\grep -RHn "$1" | grep -v '^Binary' | cut -d: -f1 | uniq -c)\n\n" | column -t; echo; }
-searchcounti(){ printf "\nMatches\tFilename\n-----\t--------------------------------\n$(\grep -iRHn "$1" | grep -v '^Binary' | cut -d: -f1 | uniq -c)\n\n" | column -t; }
+function se(){    search "$1" "*.$2";    } # Search shortcut which puts in the *. prefix to a filetype for you
+function si(){    search "$1" "*.$2";    } # Case-insensitive shortcut function
+function sai(){   search "$1" '*' 1;     } # Search all files case-insensitive
+function sphp(){  search "$1" '*.php';   } # Search PHP files
+function sphpi(){ search "$1" '*.php' 1; } # Search PHP files case-insensitive
+function scss(){  search "$1" '*.css';   } # Search CSS files
+function scssi(){ search "$1" '*.css' 1; } # Search CSS files case-insensitive
+function sjs(){   search "$1" '*.js';    } # Search JavaScript files
+function sjsi(){  search "$1" '*.js' 1;  } # Search JavaScript files case-insensitive
+
+# Search for a count of matches within each file
+function searchcount(){
+  local matches="$(command grep -RHn "$1" | grep -v '^Binary' | cut -d: -f1 | uniq -c)"
+  printf "Matches\tFilename\n-----\t--------------------------------\n%s\n" "$matches" | column -t
+}
+
+# Search for a count of case-insensitive matches within each file
+function searchcounti(){
+  local matches="$(command grep -RHni "$1" | grep -v '^Binary' | cut -d: -f1 | uniq -c)"
+  printf "Matches\tFilename\n-----\t--------------------------------\n%s\n" "$matches" | column -t
+}
